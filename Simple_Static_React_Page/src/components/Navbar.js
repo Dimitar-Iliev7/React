627986acf7cbe8f8c4affc2da0b8c7f75fa31fad
Dimitar Iliev <dimitar7iliev@yahoo.com>
2023-01-11 18:15:13 +0200

    Static_React_Page
    Simple static React Page

---
+++ Simple_Static_React_Page/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
+            className="nav--icon"
+            width="60px"
+            />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">Dimitar Iliev -Project 1</h4>
+        </nav>
+    )
+}
